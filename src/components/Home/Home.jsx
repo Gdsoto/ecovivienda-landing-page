094db1d50b16f85ab24562d02696e76061ee930a
Gerson Soto <gersonsoto5622@gmail.com>
2022-11-08 17:50:26 -0500

    Added status

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,12 +5,14 @@ import ProjectCards from './components/ProjectCards/ProjectCards';
 import Gallery from './components/Gallery/Gallery';
 import Contact from './components/Contact/Contact';
 import Slider from './components/Carousel/Carousel';
+import Objetives from './components/Objetives/Objetives';
 
 const Home = () => {
   return (
     <>
       <Slider />
       <ProjectCards />
+      <Objetives />
       <Gallery />
       <Contact />
     </>
